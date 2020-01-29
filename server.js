2b97ac0679fae88be825c72c9cbb999f411a61e2
Sebastian Kim <sydneyitguy@gmail.com>
2020-01-30 03:07:14 +0900

    No image loading

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const prerender = require('prerender');
 const server = prerender({
   port: 7777,
   pageLoadTimeout: 300 * 1000, // 5 minutes
-  chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ]
+  chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--blink-settings=imagesEnabled=false' ]
 });
 server.use(require('prerender-memory-cache'))
 server.start();
