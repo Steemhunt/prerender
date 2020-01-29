c72c72adebbf777ba55de4b93cf18858302e30e6
Sebastian Kim <sydneyitguy@gmail.com>
2020-01-30 03:05:13 +0900

    Increase timeout

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const prerender = require('prerender');
 const server = prerender({
   port: 7777,
-  pageLoadTimeout: 40 * 1000,
+  pageLoadTimeout: 300 * 1000, // 5 minutes
   chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars' ]
 });
 server.use(require('prerender-memory-cache'))
