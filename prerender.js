975b50f8a92fa1a74496f212151e6d7912f6e6e4
Sebastian Kim <sydneyitguy@gmail.com>
2020-01-30 04:39:26 +0900

    Rename

--- prerender.js
+++ prerender.js
@@ -2,7 +2,7 @@ const prerender = require('prerender');
 const server = prerender({
   port: 7777,
   pageLoadTimeout: 40 * 1000, // 40 seconds
-  chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--blink-settings=imagesEnabled=false' ]
+  chromeFlags: [ '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--blink-settings=imagesEnabled=false' ]
 });
 server.use(require('prerender-memory-cache'))
 server.start();
