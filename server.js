6127bde574f04dbf238d6b11a36ddc1685bdbfe6
Sebastian Kim <sydneyitguy@gmail.com>
2020-01-29 17:59:19 +0900

    Revert "Add option for chrome"
    This reverts commit 2172c9139988b1be18f96d8dee8dc19acb187f27.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const prerender = require('prerender');
 const server = prerender({
   port: 7777,
   workers: 8,
-  pageLoadTimeout: 60 * 1000,
-  chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--hide-scrollbars' ],
+  pageLoadTimeout: 60 * 1000
 });
 server.start();
