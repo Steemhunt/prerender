cd10afe40f80d654143fbc12bb99df007d97e560
Sebastian Kim <sydneyitguy@gmail.com>
2020-01-30 02:09:49 +0900

    1 server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const prerender = require('prerender');
 const server = prerender({
   port: 7777,
-  workers: 256,
+  workers: 1,
   pageLoadTimeout: 40 * 1000
 });
 server.use(require('prerender-memory-cache'))
