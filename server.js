2172c9139988b1be18f96d8dee8dc19acb187f27
Sebastian Kim <sydneyitguy@gmail.com>
2020-01-29 17:58:21 +0900

    Add option for chrome

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const prerender = require('prerender');
 const server = prerender({
   port: 7777,
   workers: 8,
-  pageLoadTimeout: 60 * 1000
+  pageLoadTimeout: 60 * 1000,
+  chromeFlags: [ '--no-sandbox', '--headless', '--disable-gpu', '--hide-scrollbars' ],
 });
 server.start();
