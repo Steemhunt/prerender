c3f46bf829aa2be3f24859d764251c233f1a1985
Sebastian Kim <sydneyitguy@gmail.com>
2022-11-04 17:33:55 +0900

    Update chrome flags

--- prerender.js
+++ prerender.js
@@ -2,7 +2,7 @@ const prerender = require('prerender');
 const server = prerender({
   port: 7777,
   pageLoadTimeout: 40 * 1000, // 40 seconds
-  chromeFlags: ['--headless', '--disable-gpu', '--disable-software-rasterizer', '--disable-dev-shm-usage', '--remote-debugging-port=9222', '--hide-scrollbars', '--blink-settings=imagesEnabled=false', '--crash-dumps-dir=/dev/null']
+  chromeFlags: ['--headless', '--disable-gpu', '--disable-software-rasterizer', '--disable-dev-shm-usage', '--remote-debugging-port=9222', '--hide-scrollbars', '--blink-settings=imagesEnabled=false']
 });
 server.use(require('prerender-memory-cache'))
 server.start();
